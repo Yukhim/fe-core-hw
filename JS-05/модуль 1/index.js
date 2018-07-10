16ed1a960ab61b6d13dd18bf7e38b0c16b4e19c2
Oleksandr Yukhymenko <yukhim@ukr.net>
2018-07-10 11:53:31 +0300

    Homework1 addedv1

--- JS-05/модуль 1/index.js
+++ JS-05/модуль 1/index.js
@@ -1,4 +1,25 @@
-'use strict';
-console.log("e");
-console.log(name);
+const adminLogin = 'admin';
+const adminPassword = 'm4ngo1zh4ackz0r';
+
+let userName = prompt("Enter your name");
+let userPass;
+let deny = "Действие отменено";
+let prohibited = "Доступ запещен";
+let welcome = "Добро пожаловать";
+
+if(userName===null){
+  alert(deny);
+}else if(userName!==adminLogin){
+  alert(prohibited);
+}else{
+  userPass = prompt("Enter your pass");
+}
+
+if(userPass===null){
+  alert(deny);
+}else if(userPass!==adminLogin){
+  alert(prohibited);
+}else{
+  alert(welcome);
+}
 
